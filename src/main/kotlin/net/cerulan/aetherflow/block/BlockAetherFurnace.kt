98342cc7c87e642ab548a8addd9418457c549ce4
CerulanLumina <CerulanLumina@users.noreply.github.com>
2019-12-24 17:21:57 -0500

    Add facing directions to aether furnace

--- src/main/kotlin/net/cerulan/aetherflow/block/BlockAetherFurnace.kt
+++ src/main/kotlin/net/cerulan/aetherflow/block/BlockAetherFurnace.kt
@@ -8,16 +8,36 @@ import net.minecraft.block.BlockState
 import net.minecraft.block.Material
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemPlacementContext
+import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.DirectionProperty
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 
 object BlockAetherFurnace :
-    Block(FabricBlockSettings.of(Material.STONE).nonOpaque().breakByHand(true).strength(1f, 5f).build()),
+    Block(FabricBlockSettings.of(Material.STONE).nonOpaque().breakByHand(true).strength(1f, 5f).sounds(BlockSoundGroup.METAL).build()),
     BlockEntityProvider {
+
+    object Props {
+        val ACTIVE: BooleanProperty = BooleanProperty.of("active")
+        val FACING: DirectionProperty = DirectionProperty.of("facing", Direction.NORTH, Direction.EAST, Direction.WEST, Direction.SOUTH)
+    }
+
+    init {
+        defaultState = stateManager.defaultState.with(Props.ACTIVE, false).with(Props.FACING, Direction.NORTH)
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
+        builder.add(Props.ACTIVE)
+            .add(Props.FACING)
+    }
 
     override fun createBlockEntity(view: BlockView?): BlockEntity? {
         return AetherFurnace()
@@ -36,5 +56,6 @@ object BlockAetherFurnace :
         return ActionResult.SUCCESS
     }
 
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState = defaultState.with(Props.FACING, ctx.playerFacing.opposite)
 
 }
