88bb39786e70f4bbf3ffc3fe34a3b6f6841864db
CerulanLumina <CerulanLumina@users.noreply.github.com>
2019-12-31 09:17:11 -0500

    Add textures to aether furnace gui

--- src/main/kotlin/net/cerulan/aetherflow/container/AetherFurnaceController.kt
+++ src/main/kotlin/net/cerulan/aetherflow/container/AetherFurnaceController.kt
@@ -4,12 +4,17 @@ import io.github.cottonmc.cotton.gui.CottonCraftingController
 import io.github.cottonmc.cotton.gui.widget.*
 import net.cerulan.aetherflow.recipe.AetherflowRecipeTypes
 import net.minecraft.container.BlockContext
-import net.minecraft.container.Slot
 import net.minecraft.entity.player.PlayerInventory
-import net.minecraft.item.ItemStack
 import net.minecraft.text.TranslatableText
+import net.minecraft.util.Identifier
 
 class AetherFurnaceController(syncId: Int, playerInv: PlayerInventory, context: BlockContext): CottonCraftingController(AetherflowRecipeTypes.AETHER_FURNACE, syncId, playerInv, getBlockInventory(context), getBlockPropertyDelegate(context)) {
+
+    companion object {
+        private val bar_full = Identifier("aetherflow", "textures/gui/container/progress_full.png")
+        private val bar_empty = Identifier("aetherflow", "textures/gui/container/progress_empty.png")
+    }
+
     private val inputSlot: WItemSlot
     private val outputSlot: WItemSlot
     init {
@@ -18,15 +23,14 @@ class AetherFurnaceController(syncId: Int, playerInv: PlayerInventory, context: 
         inputSlot = WItemSlot.of(blockInventory, 0)
         outputSlot = WItemSlot.of(blockInventory, 1)
 
-        val progBar = WBar(null, null, 0, 1, WBar.Direction.RIGHT)
+        val progBar = WBar(bar_empty, bar_full, 0, 1, WBar.Direction.RIGHT)
 
         rootPanel.add(progBar, 3, 1)
-        progBar.setLocation(20, 10)
-        progBar.setSize(20, 10)
-
-
         rootPanel.add(inputSlot, 2, 1)
         rootPanel.add(outputSlot, 6, 1)
+
+        progBar.setLocation(inputSlot.absoluteX + 18, inputSlot.absoluteY + 1)
+        progBar.setSize((3*18-2), (3*18-2)/4)
 
         rootPanel.add(createPlayerInventoryPanel(), 0, 3)
         rootPanel.validate(this)
